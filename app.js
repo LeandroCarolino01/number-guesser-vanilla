5d6a33566c34fef85cd63245de9b9c49dd4b6576
Leandro.Carolino <carolino.leandro@gmail.com>
2018-10-28 22:17:38 +0100

    variables for UI created

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-alert('hey');
+// game value
+let min = 4,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
+    
+// Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+//Listen for guess
