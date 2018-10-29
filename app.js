abd0c8f9a20cf747ef4188e840e9374c2e0ec190
Leandro.Carolino <carolino.leandro@gmail.com>
2018-10-29 19:23:36 +0100

    validation added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // game value
-let min = 4,
+let min = 1,
     max = 10,
     winningNum = 2,
     guessesLeft = 3;
@@ -16,3 +16,26 @@ minNum.textContent = min;
 maxNum.textContent = max;
 
 //Listen for guess
+
+guessBtn.addEventListener('click', function(){
+    let guess = parseInt(guessInput.value);
+    //console.log(guess);
+    if(isNaN(guess)  || guess < min || guess > max) {
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+    }
+
+    if(guess === winningNum){
+        guessInput.disabled = true;
+        guessInput.style.borderColor = 'green';
+        setMessage(`${winningNum} is correct`, 'green');
+    } else {
+
+    }
+
+  
+});
+
+function setMessage(msg, color) {
+    message.style.color = color;
+    message.textContent = msg;
+}
